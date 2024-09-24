9dbf47a0f37e1cd3062fd19edaf82fdc1f72a76e
carol <carol@LAPTOP-IJ353F7M>
2024-09-24 16:14:09 +0400

    Installation de Jest

--- test/script.test.js
+++ test/script.test.js
@@ -1,3 +1,9 @@
+const { TextEncoder, TextDecoder } = require('util');
+
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
+
+
 const jsdom = require('jsdom');
 const { JSDOM } = jsdom;
 
